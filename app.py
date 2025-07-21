75e4f33d84029a25e49a9bd3e557acc8fd6f63f4
Christatoui <christian_boudjaoui@apple.com>
2025-07-21 14:50:47 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -235,12 +235,12 @@ if uploaded_file is not None:
     st.sidebar.header("Graph Options")
     
     graph_options = {
-        "Hardware Request Counts": plot_hardware_counts,
         "Top 10 Requesters": plot_top_requesters,
         "Analysis by Hardware Type": plot_hardware_specific_analysis,
         "Total Requests per Month": plot_total_requests_per_month,
         "Requests by Hour (%) Average": plot_requests_by_hour,
         "Daily Requests by Weekday (Mon-Fri)": plot_daily_requests_by_weekday,
+        "Hardware Request Counts": plot_hardware_counts,
     }
 
     selected_graph = st.sidebar.selectbox("Choose a graph to display", list(graph_options.keys()))
